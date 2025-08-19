e49c781841e870b8463b0491b91525d4312ccd13
Projetodrop01 <projetodropespanha01@gmail.com>
2025-08-18 22:52:51 -0300

    Fix Vite HTML entry point

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
